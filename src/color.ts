0ffa5180dbdb9d1d0381d9b42a41c9008347f500
Brian Gann <bkgann@gmail.com>
2020-09-10 14:07:31 -0500

    fix for rgb/rgba color spec in thresholds

--- src/color.ts
+++ src/color.ts
@@ -37,6 +37,36 @@ export class Color {
     return new Color(r, g, b);
   }
 
+  RGBToHex(rgb) {
+    let sep = rgb.indexOf(",") > -1 ? "," : " ";
+    rgb = rgb.substr(4).split(")")[0].split(sep);
+    // Convert %s to 0–255
+    for (let R in rgb) {
+      let r = rgb[R];
+      if (r.indexOf("%") > -1)
+        rgb[R] = Math.round(r.substr(0, r.length - 1) / 100 * 255);
+    }
+  }
+
+  static RGBAToHex(orig: string) {
+    const rgb = orig.replace(/\s/g, '').match(/^rgba?\((\d+),(\d+),(\d+),?([^,\s)]+)?/i);
+    const alpha = (rgb && rgb[4] || "").trim();
+    let hex = rgb ?
+      (parseInt(rgb[1], 10) | 1 << 8).toString(16).slice(1) +
+      (parseInt(rgb[2], 10) | 1 << 8).toString(16).slice(1) +
+      (parseInt(rgb[3], 10) | 1 << 8).toString(16).slice(1) : orig;
+
+    var a;
+    if (alpha !== "") {
+      const alphaVal = parseFloat(alpha);
+      // multiply before convert to HEX
+      a = ((alphaVal * 255) | 1 << 8).toString(16).slice(1);
+    } else {
+      a = 1;
+    }
+    return '#' + hex + a;
+  }
+
   fromHex(hex) {
     // http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
     hex = hex.substring(1, 7);
@@ -51,7 +81,12 @@ export class Color {
     const purelight = new Color(255, 255, 255);
     for (let i = 0; i < data.length; i++) {
       const aColorStart = new Color(0, 0, 0);
-      aColorStart.fromHex(data[i].color);
+      // color can be in hex or in rgb
+      let useColor: string = data[i].color;
+      if (useColor.startsWith('rgba')) {
+        useColor = this.RGBAToHex(useColor);
+      }
+      aColorStart.fromHex(useColor);
       const aColorEnd = aColorStart.Mul(purelight, 0.7);
       gradients.push({ start: aColorStart.asHex(), end: aColorEnd.asHex() });
     }
