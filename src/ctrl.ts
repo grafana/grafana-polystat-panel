6faeb35281ed40a9bf6191686227c40f7bc1ac69
Brian Gann <bkgann@gmail.com>
2021-06-17 00:52:57 -0500

    revert check

--- src/ctrl.ts
+++ src/ctrl.ts
@@ -542,7 +542,7 @@ class D3PolystatPanelCtrl extends MetricsPanelCtrl {
     for (let index = 0; index < data.length; index++) {
       // Check for mapped value, if nothing set, format value
       const mappedValue = getMappedValue(mappings, data[index].value.toString());
-      if (mappedValue && mappedValue.text !== "") {
+      if (mappedValue) {
         data[index].valueFormatted = mappedValue.text;
       } else {
         const formatFunc = kbn.valueFormats[this.panel.polystat.globalUnitFormat];
