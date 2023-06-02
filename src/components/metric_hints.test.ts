6b39dff02e266ea022b38bd37dfacb4121ec3156
Brian Gann <bkgann@gmail.com>
2023-06-01 23:21:39 -0400

    enhanced metric hints

--- src/components/metric_hints.test.ts
+++ src/components/metric_hints.test.ts
@@ -5,6 +5,7 @@ describe('Test Metric Hints', () => {
   let wideFrame: DataFrame;
   let narrowFrame: DataFrame;
   let frameCW: DataFrame;
+  let frameInfluxDB: DataFrame;
   let time: number;
 
   beforeEach(() => {
@@ -54,31 +55,58 @@ describe('Test Metric Hints', () => {
       ],
     });
 
+    frameInfluxDB = toDataFrame({
+      name: 'changePctDay.mean { coin: btc currency: usd }',
+      fields: [
+        { name: 'Time', type: FieldType.time, values: [time, time + 1, time + 2] },
+        {
+          name: 'value',
+          type: 'number',
+          typeInfo: { frame: 'float64', nullable: true },
+          labels: {
+            "coin": "btc",
+            "currency": "usd"
+          },
+          config: {
+            displayNameFromDS: 'changePctDay.mean { coin: btc currency: usd }',
+            links: [],
+          },
+          values: [1.1, 2.2, 3.3],
+        },
+      ],
+    });
+
   });
 
 
   describe('Metric Hints', () => {
-    it('returns set of hint from labels', () => {
+    it('returns set of hints from labels', () => {
       const hints = getMetricHints([wideFrame, narrowFrame]);
       expect(hints.size).toEqual(3);
       let val = [...hints][0];
       expect(val).toEqual('A-series');
       val = [...hints][1];
-      expect(val).toEqual('B with Label');
+      expect(val).toEqual('B with Label{fake-label-b="BName"}');
       val = [...hints][2];
       expect(val).toEqual('C-series');
     });
-    it('returns hints no labels', () => {
+    it('returns hints with no labels', () => {
       const hints = getMetricHints([narrowFrame]);
       expect(hints.size).toEqual(1);
       let val = [...hints][0];
       expect(val).toEqual('C-series');
     });
-    it('returns hints from CW frame', () => {
+    it('returns hints from a CW frame', () => {
       const hints = getMetricHints([frameCW]);
       expect(hints.size).toEqual(1);
       let val = [...hints][0];
       expect(val).toEqual('5XXError');
+    });
+    it('returns hints from an influxdb source', () => {
+      const hints = getMetricHints([frameInfluxDB]);
+      expect(hints.size).toEqual(1);
+      let val = [...hints][0];
+      expect(val).toEqual('changePctDay.mean { coin: btc currency: usd }');
     });
 
   });
--- src/components/metric_hints.ts
+++ src/components/metric_hints.ts
@@ -4,7 +4,8 @@ import { FieldType } from '@grafana/data';
 export const getMetricHints = (frames: any) => {
   let metricHints = new Set<string>();
   for (let i = 0; i < frames.length; i++) {
-    let hintValue;
+    // start with empty hint
+    let hintValue = '';
     // the frame may have a name defined, start with it, fields will change it as needed
     if (frames[i]?.name) {
       hintValue = frames[i].name;
@@ -12,17 +13,30 @@ export const getMetricHints = (frames: any) => {
     // iterate over fields, get all number types and provide as hints
     for (const aField of frames[i].fields) {
       if (aField.type === FieldType.number) {
-        // update the hint to use the field Name
-        if (aField.name) {
+        // update the hint to use the field Name if we didn't get a value from above
+        if ((aField.name) && (hintValue === '')) {
           hintValue = aField.name;
         }
-        // update the hint to use the displayNameFromDS value
-        if (aField?.config && aField.config?.displayNameFromDS) {
-          hintValue = aField.config?.displayNameFromDS;
-        }
-        // lastly check for a label with __name__ and use it instead
+        // check for a label with __name__ and use it instead
         if (aField?.labels && ('__name__' in aField.labels)) {
           hintValue = aField.labels['__name__'];
+          // append the rest of the labels
+          const appendLabels: string[] = [];
+          for (const aLabel in aField.labels) {
+            if (aLabel !== '__name__') {
+              appendLabels.push(`${aLabel}="${aField.labels[aLabel]}"`);
+            }
+          }
+          if (appendLabels.length > 0) {
+            // sort them first
+            appendLabels.sort();
+            hintValue += '{' + appendLabels.join('') + '}';
+          }
+        }
+        // update the hint to use the displayNameFromDS value
+        // (the query has a specified a naming convention)
+        if (aField?.config && aField.config?.displayNameFromDS) {
+          hintValue = aField.config?.displayNameFromDS;
         }
         metricHints.add(hintValue);
       }
