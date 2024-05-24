7af27c888739af558ce37f6dbd6eaad3496b8917
Brian Gann <bkgann@gmail.com>
2024-05-24 00:12:45 -0400

    add test to create composite

---
+++ playwright/auth-bypassed/phase3-panel/create-composite.spec.ts
@@ -0,0 +1,18 @@
+import { expect, test } from '@grafana/plugin-e2e';
+
+test('test composite creation', async ({ page, panelEditPage }) => {
+  await panelEditPage.datasource.set('gdev-testdata');
+  await panelEditPage.setVisualization('Polystat');
+  await page.getByLabel('Series count', { exact: true }).click();
+  await page.getByLabel('Series count', { exact: true }).fill('10');
+  await page.getByTestId('data-testid RefreshPicker run button').click();
+  await page.getByLabel('Composites', { exact: true }).click();
+  await page.getByRole('button', { name: 'Add Composite' }).click();
+  await page.getByLabel('Composites Composites field').getByRole('textbox').nth(1).fill('Composite-A');
+  await page.getByLabel('Composites Composites field').getByRole('textbox').nth(1).press('Enter');
+  await page.getByRole('button', { name: 'Add Metric' }).click();
+  await page.locator('#cmi-field-index-0').getByRole('textbox').pressSequentially('/A.*/');
+  await page.keyboard.press('Enter');
+  await expect(panelEditPage.refreshPanel()).toBeOK();
+  await expect(panelEditPage.panel.locator.getByText('Composite-A')).toBeVisible();
+});
--- playwright/auth-bypassed/phase3-panel/create-panel.spec.ts
+++ playwright/auth-bypassed/phase3-panel/create-panel.spec.ts
@@ -3,10 +3,7 @@ import { expect, test } from '@grafana/plugin-e2e';
 test('data query should return polystat with label A-series', async ({ panelEditPage }) => {
   await panelEditPage.datasource.set('gdev-testdata');
   await panelEditPage.setVisualization('Polystat');
-  // first render will show OK in the display
-  await expect(panelEditPage.panel.locator.getByText('OK')).toBeVisible();
-  await expect(panelEditPage.refreshPanel()).toBeOK();
-  // second will show A-series
+  // panel will display A-series
   await expect(panelEditPage.refreshPanel()).toBeOK();
   await expect(panelEditPage.panel.locator.getByText('A-series')).toBeVisible();
 });
