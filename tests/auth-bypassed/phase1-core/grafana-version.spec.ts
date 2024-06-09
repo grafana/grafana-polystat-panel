232850512f7ce78bf256768f7c76da403f3efb34
Brian Gann <bkgann@gmail.com>
2024-06-08 22:07:25 -0400

    bump package and fix test

--- tests/auth-bypassed/phase1-core/grafana-version.spec.ts
+++ tests/auth-bypassed/phase1-core/grafana-version.spec.ts
@@ -3,5 +3,6 @@ import { test, expect } from '@playwright/test';
 test('Check Grafana Version from Help Button', async ({ page }) => {
   await page.goto('http://localhost:3000/', {waitUntil: 'networkidle'});
   await page.getByLabel('Help').click();
-  await expect(page.getByText('Grafana v')).toContainText('v');
+  const versionPattern = RegExp('Grafana v\\d+');
+  await expect(page.getByText(versionPattern)).toContainText('Grafana v');
 });
