98900b7106d457f3bb093ffbbd55bccbfcd4342a
Brian Gann <briangann@users.noreply.github.com>
2024-06-08 22:49:04 -0400

    Test: Adds new playwright test for composites and enables CI with playwright (#386)
    * add id to composite editor
    
    * add test to create composite
    
    * enable ci build
    
    * debug ci
    
    * debug ci
    
    * debug ci
    
    * debug playwright
    
    * debug playwright
    
    * debug playwright
    
    * debug playwright
    
    * debug playwright
    
    * debug playwright
    
    * debug playwright
    
    * update docker-compose
    
    * bump package and fix test
    
    * update docker
    
    * update gitignore
    
    * disable version check for now
    
    * remove duplicate
    
    * try limiting test versions
    
    * skip dev image
    
    * enable test

--- tests/auth-bypassed/phase1-core/grafana-version.spec.ts
+++ tests/auth-bypassed/phase1-core/grafana-version.spec.ts
@@ -2,6 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test('Check Grafana Version from Help Button', async ({ page }) => {
   await page.goto('http://localhost:3000/', {waitUntil: 'networkidle'});
+  // only works from v10+
   await page.getByLabel('Help').click();
-  await expect(page.getByText('Grafana v')).toContainText('v');
+  const versionPattern = RegExp('Grafana v\\d+');
+  await expect(page.getByText(versionPattern)).toContainText('Grafana v');
 });
