c2f17e7cbed7e13edb4ac911c0ce84642ac6cb2a
Brian Gann <bkgann@gmail.com>
2024-06-10 22:41:52 -0400

    get version from package

--- tests/auth-bypassed/phase2-installed/check-installed.spec.ts
+++ tests/auth-bypassed/phase2-installed/check-installed.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test';
 
+import packageJSON from '../../../package.json';
+
 test('Check Plugin Installed', async ({ page }) => {
   //await page.goto('http://localhost:3000/plugins');
   await page.goto('http://localhost:3000/plugins/grafana-polystat-panel', {waitUntil: 'networkidle'});
-//  const pluginVersion = page.getByText('Version', { exact: true });
- // expect(pluginVersion).toHaveText("2.1.11");
-  const pluginVersion = '2.1.12';
+  // get version from package.json
+  const pluginVersion = packageJSON.version;
   await expect(page.getByText(`Version${pluginVersion}`)).toContainText(pluginVersion);
-  //expect(found).toBeTruthy;
 });
