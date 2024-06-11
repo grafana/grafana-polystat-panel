12a323b52735572f318d7818747ff293058d4547
Brian Gann <bkgann@gmail.com>
2024-06-10 22:23:49 -0400

    bump expected version

--- tests/auth-bypassed/phase2-installed/check-installed.spec.ts
+++ tests/auth-bypassed/phase2-installed/check-installed.spec.ts
@@ -5,7 +5,7 @@ test('Check Plugin Installed', async ({ page }) => {
   await page.goto('http://localhost:3000/plugins/grafana-polystat-panel', {waitUntil: 'networkidle'});
 //  const pluginVersion = page.getByText('Version', { exact: true });
  // expect(pluginVersion).toHaveText("2.1.11");
-  const pluginVersion = '2.1.11';
+  const pluginVersion = '2.1.12';
   await expect(page.getByText(`Version${pluginVersion}`)).toContainText(pluginVersion);
   //expect(found).toBeTruthy;
 });
