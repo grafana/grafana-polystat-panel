bda0e3c862ffd39fa5a067bcd625f112aa5d8545
Brian Gann <bkgann@gmail.com>
2025-02-25 09:04:18 -0500

    handle newer catalog rendering in tests

--- tests/auth-bypassed/phase2-installed/check-installed.spec.ts
+++ tests/auth-bypassed/phase2-installed/check-installed.spec.ts
@@ -7,5 +7,5 @@ test('Check Plugin Installed', async ({ page }) => {
   await page.goto('http://localhost:3000/plugins/grafana-polystat-panel', {waitUntil: 'networkidle'});
   // get version from package.json
   const pluginVersion = packageJSON.version;
-  await expect(page.getByText(`Version${pluginVersion}`)).toContainText(pluginVersion);
+  await expect(page.getByText(`Version${pluginVersion}`).first()).toContainText(pluginVersion);
 });
