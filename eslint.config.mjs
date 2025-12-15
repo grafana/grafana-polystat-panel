25804ca0321a1430ec69cb79544482eb26e7298b
Brian Gann <bkgann@gmail.com>
2025-12-15 09:39:00 -0500

    update packages

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import { defineConfig } from 'eslint/config';
+import baseConfig from './.config/eslint.config.mjs';
+import grafanaEslintPluginPlugins from '@grafana/eslint-plugin-plugins';
+
+export default defineConfig([
+  {
+    ignores: [
+      '**/node_modules',
+      '**/dist',
+      '**/provisioning-private',
+      '**/coverage',
+      '**/.DS_Store',
+      '.vscode/*.log',
+      '.yarn/install-state.gz',
+      '**/.eslintcache',
+      '**/.idea',
+      '**/.stignore',
+      '**/blob-report/',
+      'tests/.auth',
+      'tests/.cache/',
+      '**/playwright/',
+      '**/playwright-report/',
+      '**/test-results/',
+    ],
+  },
+  ...baseConfig,
+  {
+    plugins: {
+      'grafana/plugins': grafanaEslintPluginPlugins,
+    },
+
+    rules: {
+      '@grafana/plugins/import-is-compatible': ['warn'],
+    },
+  },
+]);
