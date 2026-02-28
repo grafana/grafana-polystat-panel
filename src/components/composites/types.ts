7ef9ff861780e565c04cd74eae8572815764ecc1
Brian Gann <bkgann@gmail.com>
2026-02-28 01:10:27 -0500

    narrow composite type shim exports to legacy surface

--- src/components/composites/types.ts
+++ src/components/composites/types.ts
@@ -1,1 +1,8 @@
-export * from '@grafana/polystat-composites/editor';
+export { DisplayModes } from '@grafana/polystat-composites/editor';
+export type {
+  CompositeItemProps,
+  CompositeItemTracker,
+  CompositeItemType,
+  CompositeMetric,
+  CompositeMetricItemProps,
+} from '@grafana/polystat-composites/editor';
